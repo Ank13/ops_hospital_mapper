275cabf9e2bacc6eb9df58acb16a793f8ff38e14
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-04 18:23:29 -0500

    hospital model, csv and migration

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,7 +1,10 @@
 class Hospital < ActiveRecord::Base
+  attr_accessible :provider_id, :provider_name, :provider_street_address, :provider_city, :provider_state, :provider_zip_code, :hrr, :total_discharges, :count_drgs, :average_covered_charges, :average_total_payments, :latitude, :longitude
+  
+  set_primary_key"provider_id"
   has_many :hospitals_procedures
   has_many :procedures, :through => :hospitals_procedures
-  has_one  :patient_survey
-  has_one  :outcome
-  has_one  :complication
+  has_one  :patient_survey, :foreign_key => :provider_id, :primary_key => :provider_id
+  has_one  :outcome, :foreign_key => :provider_id, :primary_key => :provider_id
+  has_one  :complication, :foreign_key => :provider_id, :primary_key => :provider_id
 end
