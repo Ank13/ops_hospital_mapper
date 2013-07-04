6bf20f7a5e515ee3282c56767f84f279e2185bd4
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-04 10:59:39 -0500

    loads Chicago map on opening

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,10 +1,15 @@
-function initialize() {
-var mapOptions = {
-  center: new google.maps.LatLng(-34.397, 150.644),
-  zoom: 8,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+$(document).ready(function(){
+  
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(41.8500, -87.6500),
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+    };
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+});
