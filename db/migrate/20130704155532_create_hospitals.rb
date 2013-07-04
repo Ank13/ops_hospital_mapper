1c6b2f423ece6aad8a6f8ea84e0c550fe9ae614a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-04 15:09:01 -0500

    migration files with headers from csv

--- db/migrate/20130704155532_create_hospitals.rb
+++ db/migrate/20130704155532_create_hospitals.rb
@@ -1,14 +1,19 @@
 class CreateHospitals < ActiveRecord::Migration
 	def change
 		create_table :hospitals do |t|
-			t.string :name
 			t.integer :provider_id
-			t.string :address
-			t.string :city
-			t.string :state
-			t.string :hrr
-			t.float :latitude
-			t.float :longitude
+	    t.string  :provider_name
+	    t.string  :provider_street_address
+	    t.string  :provider_city
+	    t.string  :provider_state
+	    t.integer :provider_zip_code
+	    t.string  :hrr
+	    t.integer :total_discharges 
+	    t.integer :count_drgs
+	    t.float   :average_covered_charges 
+			t.float	  :average_total_payments 
+			t.float	  :latitude
+			t.float	  :longitude
 
 			t.timestamps
 		end
