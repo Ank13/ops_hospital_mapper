87575a77885d6a3782b56141029687a0a83d8d50
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-04 17:27:38 -0500

    adds info boxes for each marker

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
       zoom: 11,
       mapTypeId: google.maps.MapTypeId.TERRAIN
     };
+
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
 
@@ -15,9 +16,9 @@ $(document).ready(function(){
 
   // TODO: Read this in from database
     var hospitals = [
-      ['DBC', 41.889911, -87.637657, 4],
-      ["Children's", 41.874186, -87.668502, 1],
-      ['Northwestern', 41.908165, -87.648534, 2]
+      ['DBC', 41.889911, -87.637657, 3, "DBC content"],
+      ["Children's", 41.874186, -87.668502, 1, "Children's content"],
+      ['Northwestern', 41.908165, -87.648534, 2, "Northwestern content"]
     ];
 
     function setMarkers(map, locations){
@@ -39,12 +40,19 @@ $(document).ready(function(){
           shape: shape,
           title: hospital[0],
           zIndex: hospital[3]
-      });
+        });
+         marker.html = hospital[4];
 
+        var infowindow = new google.maps.InfoWindow({
+          maxWidth: 200 
+        });
+   
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(this.html);
+          infowindow.open(map, this);
+        });
+      };
     };
-
-  };
-
   google.maps.event.addDomListener(window, 'load', initialize);
 
 });
