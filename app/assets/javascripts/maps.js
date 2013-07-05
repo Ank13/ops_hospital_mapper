c0cd149a512fbf73125095b0b80d4098a080ed14
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-05 17:08:47 -0500

    added location information to div

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  var hospitals = $("#hospitals").data("hospitals");
+
   function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(41.8500, -87.8500),
@@ -14,13 +16,6 @@ $(document).ready(function(){
 
   };
 
-  // TODO: Read this in from database
-    var hospitals = [
-      ['DBC', 41.889911, -87.637657, 3, "DBC content"],
-      ["Children's", 41.874186, -87.668502, 1, "Children's content"],
-      ['Northwestern', 41.908165, -87.648534, 2, "Northwestern content"]
-    ];
-
     function setMarkers(map, locations){
       
       var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
@@ -32,16 +27,16 @@ $(document).ready(function(){
 
       for (var i = 0; i < locations.length; i++) {
         var hospital = locations[i];
-        var myLatLng = new google.maps.LatLng(hospital[1], hospital[2]);
+        var myLatLng = new google.maps.LatLng(hospital["latitude"], hospital["longitude"]);
         var marker = new google.maps.Marker({
           position: myLatLng,
           map: map,
           icon: iconBase + 'hospitals_maps.png',
           shape: shape,
-          title: hospital[0],
-          zIndex: hospital[3]
+          title: hospital["provider_name"],
+          zIndex: i
         });
-         marker.html = hospital[4];
+         marker.html = hospital["provider_name"];
 
         var infowindow = new google.maps.InfoWindow({
           maxWidth: 200 
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,6 +1,20 @@
 class MapsController < ApplicationController
 
   def index
+    hospitals = Hospital.all
+
+    @all_hospitals = []
+
+    hospitals.each do |hospital|
+      @all_hospitals << hospital.as_json(only: [:provider_name, :latitude, :longitude, :provider_id])
+    end
+
+    # @all_hospitals
   end
 
 end
+
+# [
+#  {"latitude"=>37.7308956, "longitude"=>-88.5248335, "provider_id"=>140210, "provider_name"=>"HARRISBURG MEDICAL CENTER"}, 
+#  {"latitude"=>41.6781639, "longitude"=>-87.6376566, "provider_id"=>140304, "provider_name"=>"ADVENTIST BOLINGBROOK HOSPITAL"}
+# ]
