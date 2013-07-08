0b432403a0c72d7b4c1ea05d6473c860d2716e33
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-08 18:14:42 -0500

    capybara & js/google not friendly

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -16,21 +16,31 @@ describe "A user can" do
 			page.should have_content 'WTF'
 		end
 
-		it "displays details nav icon" do
-			page.should have_content "Details"
-
+		it "displays tabs nav icon" do
+			page.should have_content "search by"
 		end
 
-		it "hides left side bar when details clicked" do
-			click_link 'Details'
-			page.should_not have_content 'PATIENT SURVEYS'
+		it "hides left side bar when details clicked", :js=> true do
+			sleep(1)
+			click_link 'search by'
+			sleep(5)
+			page.should_not have_content 'procedures'
+		end
 
+		it 'shows red circles when average cost is clicked', :js=> true do
+			sleep(3)
+			click_link 'Average Cost (all procedures)'
+			page.should have_css("img", :src => "https://maps.gstatic.com/mapfiles/transparent.png")
 		end
 
 		it "displays correct information when static page clicked" do
 			click_link 'datasets'
 			page.should have_content 'CMS'
 		end
-	end		
 
+		it "should display google location search box" do
+			page.should have_content "hospital or location"
+		end
+	end
 end
+
