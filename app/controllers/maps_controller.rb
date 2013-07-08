a1ae8dd7269e010524460fd4b3806f16178c7f54
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-08 18:51:05 -0500

    adds server side verification that hospital hasn't already been added to map before sending to view

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,19 +1,9 @@
 class MapsController < ApplicationController
 
-  def index
-    lat ||= 41.7700
-    long ||= -87.9000
-    distance ||= 5
-    box = Geocoder::Calculations.bounding_box([lat, long], distance)
+  @@loaded = {} 
 
-    hospitals = Hospital.geocoded.within_bounding_box(box)
+  def index
     @procedures = Procedure.all
-    @all_hospitals = []
-    hospitals.each do |hospital|
-      acc = hospital.average_covered_charges/1600
-      @all_hospitals << {provider_name: hospital.provider_name, latitude: hospital.latitude, longitude: hospital.longitude, 
-        provider_id: hospital.provider_id, infobox_html: hospital.infobox_html_on_load, acc: acc}
-    end
   end
 
   def more_markers
@@ -26,8 +16,11 @@ class MapsController < ApplicationController
     data = []
     hospitals.each do |hospital|
       acc = hospital.average_covered_charges/1600
-      data << {provider_name: hospital.provider_name, latitude: hospital.latitude, longitude: hospital.longitude, 
-        provider_id: hospital.provider_id, infobox_html: hospital.infobox_html_on_load, acc: acc}
+      if !@@loaded.has_key?(hospital.provider_id)
+        data << {provider_name: hospital.provider_name, latitude: hospital.latitude, longitude: hospital.longitude, 
+          provider_id: hospital.provider_id, infobox_html: hospital.infobox_html_on_load, acc: acc}
+        @@loaded[hospital.provider_id] = true
+      end
     end
 
     respond_to do |format|
