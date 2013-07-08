f01ff55cc701e65bac3f486063d730f7129ab4e2
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-08 08:34:56 -0500

    generalize graph in js to accept different data

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -18,6 +18,7 @@ class Hospital < ActiveRecord::Base
   def infobox_html_on_load
     unless self.patient_survey.nil? && self.outcome.nil?
       return "<div class='info-box'> #{provider_name} </div>
+
               <p><a class='info-comparison' id='#{provider_id}' href='#'>Comparison Details</a></p>
               <p><a class='info-outcomes' id='#{provider_id}' href='#'>Outcomes</a></p>
               <ul>
@@ -46,7 +47,7 @@ class Hospital < ActiveRecord::Base
     procedure = Procedure.find_by_drg_id(drg).drg_def
     il_charge = (Procedure.find_by_drg_id(drg).avg_covered_charges_IL/1000)
     natl_charge = (Procedure.find_by_drg_id(drg).natl_avg_total_payments/1000)
-    {hospital_charge: hospital_charge, il_charge: il_charge, natl_charge: natl_charge, hospital_id: provider_id, procedure: procedure}
+    {first_col: hospital_charge, second_col: il_charge, third_col: natl_charge, hospital_id: provider_id, procedure: procedure}
   end
 
   def complication_cost_correlation
@@ -69,13 +70,13 @@ class Hospital < ActiveRecord::Base
 
   def outcomebox_on_click
     unless self.complication.R_D_S_T_C.nil?
-      hosp_complication_rate = self.complication.R_D_S_T_C
+      hosp_complication_rate = self.complication.R_D_S_T_C.to_i
     else
       hospital_charge = 0
     end
     avg_complication_rate = Complication.average("R_D_S_T_C").to_i
     
-    {hosp_complication_rate: hosp_complication_rate, avg_complication_rate: avg_complication_rate}
+    {first_col: hosp_complication_rate, second_col: avg_complication_rate}
   end
 
 
