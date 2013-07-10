e396e92335874ea150ebd0ebeb9b411ac2a2b59b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-10 15:28:50 -0500

    working on stnd dev

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,4 +1,6 @@
 class Procedure < ActiveRecord::Base
+  include Stats
+
   attr_accessible :drg_id, :drg_def, :total_discharges_IL, :avg_covered_charges_IL, :avg_total_payments_IL, :natl_total_discharges, :natl_avg_covered_charges, :natl_avg_total_payments, :description, :clinical_category
 
 	self.primary_key = "drg_id"
@@ -8,15 +10,21 @@ class Procedure < ActiveRecord::Base
 
 
   def most_expensive_hospital
-    hospitals_procedures.order('avg_covered_charges DESC').limit(1).first.provider_name
+    hospitals_procedures.order('avg_covered_charges DESC').limit(1).first
   end
 
   def least_expensive_hospital
-    hospitals_procedures.order('avg_covered_charges ASC').limit(1).first.provider_name
+    hospitals_procedures.order('avg_covered_charges ASC').limit(1).first
   end
 
   def average_cost
     hospitals_procedures.average('avg_covered_charges').to_i
+  end
+
+  def stand_dev
+    allhosp = []
+    hospitals_procedures.all.each{|hp| allhosp << hp.avg_covered_charges}
+    allhosp.standard_deviation
   end
 
 end
