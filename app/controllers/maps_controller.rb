bf809988cbae120243c5a55950c40ee729873bc2
Daniel Song <danielx328@gmail.com>
2013-07-10 08:43:14 -0500

    WIP: 2nd panel

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -30,8 +30,9 @@ class MapsController < ApplicationController
   end
   
   def analytics
-    definition = params[:drg].upcase
-    @procedure = Procedure.find_by_drg_def(definition)
+    id = params[:drg][0,2]
+    p id
+    @procedure = Procedure.find_by_drg_id(id)
 
     render :layout => false
   end
