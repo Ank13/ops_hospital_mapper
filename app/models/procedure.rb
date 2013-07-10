ea7a633af0accd1ab4ae7f0d382c153dfa54a085
codesliced <codesliced@gmail.com>
2013-07-10 15:40:34 -0500

    set primary keys for models

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,7 +1,7 @@
 class Procedure < ActiveRecord::Base
   attr_accessible :drg_id, :drg_def, :total_discharges, :avg_covered_charges, :avg_total_payments, :natl_total_discharges, :natl_avg_covered_charges, :natl_avg_total_payments, :description, :clinical_category
 
-	# self.primary_key = "drg_id"
+	self.primary_key = "drg_id"
   has_many :hospitals_procedures, :foreign_key => :drg_id
 	has_many :hospitals, :through => :hospitals_procedures, :primary_key => :drg_id
   has_many :states_procedures, :foreign_key => :drg_id
