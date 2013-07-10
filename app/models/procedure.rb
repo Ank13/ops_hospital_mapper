ccd4c0f7db20b404d55909de711401da2e80a1af
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-10 16:21:06 -0500

    finished basic stats

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,8 +1,8 @@
 class Procedure < ActiveRecord::Base
 
-  attr_accessible :drg_id, :drg_def, :total_discharges, :avg_covered_charges, :avg_total_payments, :natl_total_discharges, :natl_avg_covered_charges, :natl_avg_total_payments, :description, :clinical_category
+  include StatsHelper
 
-  include Stats
+  attr_accessible :drg_id, :drg_def, :total_discharges, :avg_covered_charges, :avg_total_payments, :natl_total_discharges, :natl_avg_covered_charges, :natl_avg_total_payments, :description, :clinical_category
 
 	self.primary_key = "drg_id"
   has_many :hospitals_procedures, :foreign_key => :drg_id
