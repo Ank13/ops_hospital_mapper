b37d5f9d1b2d141a63a8acff2251d4214d1d4119
Daniel Song <danielx328@gmail.com>
2013-07-10 10:21:52 -0500

    separated toggle for procedure

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -23,20 +23,22 @@ $(document).ready(function(){
     return false;
   });
 
-  $(".trigger2").click(function(){
-    $(this).toggleClass("active");
-    if($(this).hasClass("active")) {
+  $(".trigger2").click(secondTab());
+
+  function secondTab() {
+    $('.trigger2').toggleClass("active");
+    if($('.trigger2').hasClass("active")) {
       $('#analytics').animate({left: '+=250'}, 120);
-      $(this).animate({left: '+=240'}, 120);
+      $('.trigger2').animate({left: '+=240'}, 120);
       $(".slidepanel").css('z-index', '3').show("fast");
     } 
     else {
       $('#analytics').animate({left: '-=250'}, 120);
-      $(this).animate({left: '-=240'}, 120);
+      $('.trigger2').animate({left: '-=240'}, 120);
       $(".slidepanel").hide("fast");
     }
     return false;
-  });
+  };
 
   function triggerStaticPages(link, page) {
     $(link).click(function(e) {
