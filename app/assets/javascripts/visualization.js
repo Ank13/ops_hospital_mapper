7aef0a0361a5fb9fc204106f41e2d6bdf447ec4e
Daniel Song <danielx328@gmail.com>
2013-07-11 16:12:07 -0500

    icon colors on clicked, not hover

--- app/assets/javascripts/visualization.js
+++ app/assets/javascripts/visualization.js
@@ -1,23 +1,40 @@
 $(document).ready(function() {
 	for(var i=0; i < collection.length; i++) {
-		changeColorWhenHover(collection[i]['div'], collection[i]['icon'], collection[i]['color']);
+		changeColorWhenMouse(collection[i]['div'], collection[i]['icon'], collection[i]['color']);
 	}
+	changeClearIconColorOnHover();
+
+	function changeColorWhenMouse(div, icon, color) {
+		$(div).on("click", function() {
+			$(this).toggleClass('active');
+			if($(this).hasClass('active')) {
+				$(icon).css('color', color);
+			}
+			else {
+				$(icon).css('color', '#183152');
+			}
+		});
+		$('.clearAll').click(function() {
+			$(div).removeClass('active');
+			$(icon).css('color', '#183152');
+		});
+	};
+
+	function changeClearIconColorOnHover() {
+		$('.clearAll').mouseover(function() {
+			$('.icon-ban-circle').css('color', '#444');
+		});
+		$('.clearAll').mouseout(function() {
+			$('.icon-ban-circle').css('color', '#183152');
+		});
+	};
 });
 
-function changeColorWhenHover(div, icon, color) {
-	$(div).mouseover(function() {
-		$(icon).css('color', color);
-	});
-	$(div).mouseout(function() {
-		$(icon).css('color', '#183152');
-	});
-}
-
 var collection = [
-		{div: '.acc', icon: '.icon-tag', color: '#236905'},
-		{div: '.th-up', icon: '.icon-thumbs-up', color: '#EEF000'},
-		{div: '.th-down', icon: '.icon-thumbs-down', color: '#F04400'},
-		{div: '.amb', icon: '.icon-ambulance', color: '#70070A'},
-		{div: '.ghost', icon: '.icon-frown', color: 'purple'},
-		{div: '.clearAll', icon: '.icon-ban-circle', color: '#444'}
+	{div: '.acc', icon: '.icon-tag', color: '#236905'},
+	{div: '.th-up', icon: '.icon-thumbs-up', color: '#EEF000'},
+	{div: '.th-down', icon: '.icon-thumbs-down', color: '#F04400'},
+	{div: '.amb', icon: '.icon-ambulance', color: '#70070A'},
+	{div: '.ghost', icon: '.icon-frown', color: 'purple'},
 ];
+
