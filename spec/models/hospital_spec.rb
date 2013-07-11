562526d340f1b323cf917ba70604440bd9b4995e
Daniel Song <danielx328@gmail.com>
2013-07-11 13:49:21 -0500

    WIP hospital model spec

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -13,7 +13,8 @@ describe Hospital do
 	context 'public methods' do
 		before(:each) do
 			@hospital = FactoryGirl.create(:hospital)
-			@hospitals_procedure = FactoryGirl.create(:hospitals_procedure)
+			@hospital.complication = FactoryGirl.create(:complication)
+			@hospital.hospitals_procedures = FactoryGirl.create(:hospitals_procedure)
 			@procedure = FactoryGirl.create(:procedure)
 			@states_procedure = FactoryGirl.create(:states_procedure)
 		end
@@ -22,12 +23,12 @@ describe Hospital do
 			@hospital.full_address.should eq "1234 Mercy Way, Chicago, IL, 60669"
 		end
 
-		# it 'should output correct json data when infobox is clicked' do
-		# 	@hospital.infobox_on_click(1).should eq {y_axis:'Cost', first_col: 0.12345, second_col: 0.12345, third_col: 0.02345, state: 'IL', hospital_id: 1, title: "hip replacement"}
-		# end
+		it 'should output correct json data when infobox is clicked' do
+			@hospital.infobox_on_click(1).should eq {y_axis:'Cost', first_col: 0.12345, second_col: 0.12345, third_col: 0.02345, state: 'IL', hospital_id: 1, title: "hip replacement"}
+		end
 
 		it 'should return correct hospital complication rate' do
-			@hospital.complicationbox_on_click.should eq(10)
+			@hospital.complicationbox_on_click.should eq({y_axis: 'Mortality', first_col: 10, second_col: 10, third_col: 0, title: '30 Day Mortality from Serious Treatable Complications (per 1000 admissions)'})
 		end
 
 		it 'should return correct scaled total cost index' do
